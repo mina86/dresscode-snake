50f3a37be9bb95dcb0a7ae0e1d798d74c53d22c8
Michal Nazarewicz <mina86@mina86.com>
2016-05-06 00:04:25 +0200

    Show status of any updated files

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ if ! [ -d gh-pages ]; then
 fi
 
 ec=0
+found=false
 
 traverse() {
 	for name in "$1"/*; do
@@ -18,15 +19,21 @@ traverse() {
 		if [ -d "$name" ]; then
 			mkdir -p -- "${name#gh-pages/}"
 			traverse "$name"
-		elif [ -f "$name" ]; then
-			cp -p -- "$name" "${name#gh-pages/}"
-			git add "${name#gh-pages/}"
-		else
+		elif ! [ -f "$name" ]; then
 			echo "$name: dunno what to do with it" >&2
 			ec=1
+		elif ! cmp "$name" "${name#gh-pages/}" >/dev/null 2>&1; then
+			cp -p -- "$name" "${name#gh-pages/}"
+			git add "${name#gh-pages/}"
+			found=true
 		fi
 	done
 }
 
 traverse gh-pages .
+if $found; then
+	git status
+else
+	echo 'No changed files found'
+fi
 exit $ec
